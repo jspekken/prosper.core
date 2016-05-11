8c340ab49b646b9972533a93f32718c932c2af0a
Jelle Spekken <jelle.spekken@centagon.com>
2016-05-11 15:15:22 +0200

    route naming added

--- src/Prosper/Core/Http/routes/modules.php
+++ src/Prosper/Core/Http/routes/modules.php
@@ -9,10 +9,37 @@
  * file that was distributed with this source code.
  */
 
-app('router')->get('module/{module}',           'AdminController@index');
-app('router')->get('module/{module}/create',    'AdminController@create');
-app('router')->post('module/{module}',          'AdminController@store');
-app('router')->get('module/{module}/{id}',      'AdminController@show');
-app('router')->get('module/{module}/{id}/edit', 'AdminController@edit');
-app('router')->put('module/{module}/{id}',      'AdminController@update');
-app('router')->delete('module/{module}',        'AdminController@destroy');
+app('router')->get('module/{module}', [
+    'as'   => prosper_route_string('module.index'),
+    'uses' => 'AdminController@index'
+]);
+
+app('router')->get('module/{module}/create', [
+    'as'   => prosper_route_string('module.create'),
+    'uses' => 'AdminController@create'
+]);
+
+app('router')->post('module/{module}', [
+    'as'   => prosper_route_string('module.store'),
+    'uses' => 'AdminController@store'
+]);
+
+app('router')->get('module/{module}/{id}', [
+    'as'   => prosper_route_string('module.show'),
+    'uses' => 'AdminController@show'
+]);
+
+app('router')->get('module/{module}/{id}/edit', [
+    'as'   => prosper_route_string('module.edit'),
+    'uses' => 'AdminController@edit'
+]);
+
+app('router')->put('module/{module}/{id}', [
+    'as'   => prosper_route_string('module.update'),
+    'uses' => 'AdminController@update'
+]);
+
+app('router')->delete('module/{module}', [
+    'as'   => prosper_route_string('module.destroy'),
+    'uses' => 'AdminController@destroy'
+]);
