06f248ebc91ff7307f453e527b4eda96ce62086a
Jelle Spekken <jelle.spekken@centagon.com>
2016-05-11 13:39:22 +0200

    gulpfile cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,9 @@ elixir(function(mix) {
         'jquery/dist/jquery.js'
     ], 'public/js/vendor.js', 'resources/assets/components');
 
-    mix.sass('core.scss')
-        .copy('public/css/core.css', __dirname + '/../../../public/packages/prosper/core/css/core.css');
+    mix.sass('core.scss');
+    mix.copy('public/css/core.css', __dirname + '/../../../public/packages/prosper/core/css/core.css');
 
-    mix
-        .browserify('core.js')
-        .copy('public/js/core.js', __dirname + '/../../../public/packages/prosper/core/js/core.js');
+    mix.browserify('core.js');
+    mix.copy('public/js/core.js', __dirname + '/../../../public/packages/prosper/core/js/core.js');
 });
