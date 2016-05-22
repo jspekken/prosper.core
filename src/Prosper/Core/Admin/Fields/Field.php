2bfb85b18c614fd9099308715ae98b4734f38531
Jelle Spekken <jspekken@gmail.com>
2016-05-22 22:26:35 +0200

    field getters and setters pass the field instance instead of the value

--- src/Prosper/Core/Admin/Fields/Field.php
+++ src/Prosper/Core/Admin/Fields/Field.php
@@ -69,7 +69,7 @@ abstract class Field
     {
         // Try to call the property setter methods.
         if (method_exists($this, $method = 'get' . studly_case($property))) {
-            return $this->$method();
+            return $this->$method($this);
         }
 
         return $this->properties[$property];
@@ -88,7 +88,7 @@ abstract class Field
     {
         // Try to call the property setter methods.
         if (method_exists($this, $method = 'set' . studly_case($property))) {
-            $value = $this->$method($value);
+            $value = $this->$method($this);
         }
 
         $this->properties[$property] = $value;
