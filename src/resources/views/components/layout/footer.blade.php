b36a016860bf9eba813bf6b27271da4eaf985d54
Jelle Spekken <jspekken@gmail.com>
2016-06-01 00:32:00 +0200

    temp asset removal ifo faster dev

--- src/resources/views/components/layout/footer.blade.php
+++ src/resources/views/components/layout/footer.blade.php
@@ -1,3 +1,6 @@
 
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.2/js/bootstrap.min.js"></script>
+	<script src="/packages/prosper/core/js/core.js"></script>
+
 </body>
 </html>
--- src/resources/views/components/layout/header.blade.php
+++ src/resources/views/components/layout/header.blade.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <title>Document</title>
 
-    <link rel="stylesheet" href="/packages/prosper/core/css/core.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/packages/prosper/core/css/core.css">
 
 </head>
 <body>
